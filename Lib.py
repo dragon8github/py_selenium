574e63d39b1680478ac6f121dfe6c8c8919cb0e9
dragon8github <928532756@qq.com>
2017-08-21 15:30:23 +0800

    123

--- Lib.py
+++ Lib.py
@@ -20,28 +20,34 @@ class Lib:
         sleep(time)
         return method(self.driver)
 
+    # 先执行指定的事情，然后再
+    def afterSleep(self, time, method):
+        value = method(self)
+        sleep(time)
+        return value
+
     # 等待Javascript加载完成，也算是等待页面加载完毕
     # 判断方式：.page-sidebar .has-sub.active存在说明js已经加载完毕，并且完成渲染才会存在。
     # 这是自己分析的。如果以后后台代码发生了变化，规则不再如此。可以想办法替换别的规则来判断是否加载完毕
     def waitForJsLoadFinish(self):
-        return WebDriverWait(self.driver, 10).until(lambda x: x.find_element_by_css_selector(".page-sidebar .has-sub.active").is_displayed())
+        return WebDriverWait(self.driver, 30).until(lambda x: x.find_element_by_css_selector(".page-sidebar .has-sub.active").is_displayed())
 
-    def waitForElementByCss(self, path, time = 10):        
+    def waitForElementByCss(self, path, time = 30):        
         return WebDriverWait(self.driver, time).until(lambda x: x.find_element_by_css_selector(path))
 
-    def waitForElementById(self, path, time = 10):        
+    def waitForElementById(self, path, time = 30):        
         return WebDriverWait(self.driver, time).until(lambda x: x.find_element_by_id(path))
 
-    def waitForElementByXpath(self, path, time = 10):        
+    def waitForElementByXpath(self, path, time = 30):        
         return WebDriverWait(self.driver, time).until(lambda x: x.find_element_by_xpath(path))
 
-    def waitForElementDisplayByCss(self, path,  time = 10):
+    def waitForElementDisplayByCss(self, path, time = 30):
         return WebDriverWait(self.driver, time).until(lambda x: x.find_element_by_css_selector(path).is_displayed())
 
-    def waitForElementDisplayById(self, path,  time = 10):
+    def waitForElementDisplayById(self, path, time = 30):
         return WebDriverWait(self.driver, time).until(lambda x: x.find_element_by_id(path).is_displayed())
 
-    def waitForElementDisplayByXpath(self, path,  time = 10):
+    def waitForElementDisplayByXpath(self, path, time = 30):
         return WebDriverWait(self.driver, time).until(lambda x: x.find_element_by_xpath(path).is_displayed())
 
 
--- addCarBusiness.py
+++ addCarBusiness.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.webdriver.support.wait import WebDriverWait
-from time import sleep
 from Lib import Lib
 import unittest
 
@@ -22,15 +15,15 @@ class AddCarBusiness(unittest.TestCase):
     # 开始
     def test_start(self):
         # 登录
-        self.L.login("linxue", "123456@a")
+        self.L.login("chaijibing", "123456@a")
 
         # 等待左侧菜单栏“车易贷管理”出现之后才点击
         self.L.waitForElementByXpath('/html/body/div[4]/div[1]/div/ul/li[3]/ul/li[2]/a').click()
         
         # 等待js加载完毕
         if self.L.waitForJsLoadFinish():
-            # 等待 “新增” 按钮出现 并且 点击“添加”按钮
-            self.L.waitForElementByCss('#add').click()
+                # 等待 “新增” 按钮出现 并且 点击“添加”按钮
+                self.L.waitForElementByCss('#add').click()
 
         # 等待iframe并且将当前窗口切换为该iframe
         self.driver.switch_to.frame(self.L.waitForElementByCss('#layui-layer1 iframe'))
@@ -39,8 +32,8 @@ class AddCarBusiness(unittest.TestCase):
         bool_display = self.L.waitForElementDisplayById('model_tb_business_base_info_BUSINESS_ID')
         if bool_display:
             Select(self.driver.find_element_by_id("business_type_detail")).select_by_visible_text(u"GPS抵押押证")
-            self.driver.find_element_by_id("model_tb_business_base_info_CUSTOMER_NAME").send_keys(u"紫苏八一五6")
-            self.driver.find_element_by_id("model_tb_business_base_info_PHONE_NUMBER").send_keys("15876366685")
+            self.driver.find_element_by_id("model_tb_business_base_info_CUSTOMER_NAME").send_keys(u"李钊鸿自动化UI测试")
+            self.driver.find_element_by_id("model_tb_business_base_info_PHONE_NUMBER").send_keys("13713332652")
             # 虽然可能性不高，但真的可能框架的JS还没加载好。导致点击无效。
             # 但我又没有必要为了这东西写一个js加载判断。更何况这很难。所以还是用sleep 1秒吧
             self.L.sleep(1, lambda x: x.find_element_by_id("submit").click())
@@ -50,11 +43,25 @@ class AddCarBusiness(unittest.TestCase):
 
         # 截取订单号
         businessId = 'TD' + success_text.split("TD")[1]
-
+        
+        # 执行下一步任务
         self.foo(businessId)
 
-    def foo(self, id): 
-        print(id)
+    def foo(self, id):
+        # 切换回主层面
+        self.driver.switch_to.default_content()
+        # 关闭目前所在的所有layer. 其实可以直接执行指定的编辑页面js代码：EditBusiness('车易贷'," + id + ")
+        self.driver.execute_script("window.layer.closeAll();")
+        # 在搜索栏输入id
+        self.driver.find_element_by_id("carBusinessId").send_keys(id)
+        # 点击搜索
+        self.driver.find_element_by_css_selector('#search .btn').click()
+        # 等待搜索按钮出现，然后点击它
+        self.L.waitForElementByCss('.table > tbody > tr td:nth-child(10) > a').click()    
+        # 等待iframe并且将当前窗口切换为该iframe
+        self.driver.switch_to.frame(self.L.waitForElementByCss('#layui-layer1 iframe'))
+        # 等待一个编辑按钮出来 
+        self.L.waitForElementByCss("table#tableCustomer > tbody > tr:nth-child(1) > td:nth-child(7) > a[title='编辑']").click()
 
     # def tearDown(self):
         # self.driver.quit()

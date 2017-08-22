b8e00efd2e102bff60c9362eccca34e5b5317281
dragon8github <928532756@qq.com>
2017-08-22 17:58:57 +0800

    123

--- addCarBusiness.py
+++ addCarBusiness.py
@@ -22,27 +22,27 @@ class AddCarBusiness(unittest.TestCase):
         
         # 等待js加载完毕
         if self.L.waitForJsLoadFinish():
-            # 等待 “新增” 按钮出现 并且 点击“添加”按钮
-            self.L.waitForElementByCss('#add').click()
+           # 等待 “新增” 按钮出现 并且 点击“添加”按钮
+           self.L.waitForElementByCss('#add').click()
 
-            # 等待iframe并且将当前窗口切换为该iframe
-            self.driver.switch_to.frame(self.L.waitForElementByCss('#layui-layer1 iframe'))
+           # 等待iframe并且将当前窗口切换为该iframe
+           self.driver.switch_to.frame(self.L.waitForElementByCss('#layui-layer1 iframe'))
 
-            # 设置表单的值
-            if self.L.waitForElementDisplayById('model_tb_business_base_info_BUSINESS_ID'):
-                self.L.waitForSelectTextById('business_type_detail', 'GPS抵押押证')
-                self.L.setValueById("model_tb_business_base_info_CUSTOMER_NAME", "李钊鸿自动化UI测试")
-                self.L.setValueById("model_tb_business_base_info_PHONE_NUMBER", "13713332652")
-                self.L.sleep(0.3, lambda x: x.find_element_by_id("submit").click())
+           # 设置表单的值
+           if self.L.waitForElementDisplayById('model_tb_business_base_info_BUSINESS_ID'):
+              self.L.waitForSelectTextById('business_type_detail', 'GPS抵押押证')
+              self.L.setValueById("model_tb_business_base_info_CUSTOMER_NAME", "李钊鸿自动化UI测试")
+              self.L.setValueById("model_tb_business_base_info_PHONE_NUMBER", "13713332652")
+              self.L.sleep(0.3, lambda x: x.find_element_by_id("submit").click())
 
-            # 获取成功反馈
-            success_text = self.L.waitForElementByCss('#jbox #jbox-content').text
+           # 获取成功反馈
+           success_text = self.L.waitForElementByCss('#jbox #jbox-content').text
 
-            # 截取订单号
-            businessId = 'TD' + success_text.split("TD")[1]
+           # 截取订单号
+           businessId = 'TD' + success_text.split("TD")[1]
 
-            # 执行下一步任务: 编辑车易贷订单信息
-            self.EditCarBusiness(businessId)
+           # 执行下一步任务: 编辑车易贷订单信息
+           self.EditCarBusiness(businessId)
 
     def EditCarBusiness(self, id):
         # 切换回主层面
@@ -57,34 +57,34 @@ class AddCarBusiness(unittest.TestCase):
         self.L.waitForElementByCss('.table > tbody > tr td:nth-child(10) > a').click()    
         # 等待iframe并且将当前窗口切换为该iframe
         self.driver.switch_to.frame(self.L.waitForElementByCss('#layui-layer1 iframe'))
-        # 等待一个【编辑】按钮出来
+        # 等待一个【编辑】按钮出来(TODO: 这里有一个坑还没有解决)
         self.L.waitForElementByCss("table#tableCustomer > tbody > tr:nth-child(1) > td:nth-child(7) > a[title='编辑']").click()
 
         # 设置表单
         if self.L.waitForElementDisplayById('model_tb_car_personal_id_card_no'):
-            self.L.setValueById("model_tb_car_personal_id_card_no", "120311199606068814")
-            self.L.setValueById("model_tb_car_personal_nativeplace", "汉")
-            self.L.setValueById("model_tb_car_personal_current_address", "详细地址")
-            self.L.setValueById("model_tb_car_job_company_name", "单位全称")
-            self.L.setValueById("model_tb_car_job_company_address", "单位地址")
-            self.L.setValueById("model_tb_car_job_company_phone", "13214785214")
-            self.L.setValueById("model_tb_car_job_job_duty", "职位")
-            self.L.setValueById("model_tb_car_job_job_pay", "123654")
-            self.L.setValueById("model_tb_car_job_other_pay", "369852")
-            self.L.setValueById("multiple_customer_bank_info_bank_account", "62534442511225448")
-            self.L.setValueById("multiple_customer_bank_info_phone_number", "15876366685")
-            self.L.setValueById("multiple_customer_bank_info_bank_subname", "工商支行")
+           self.L.setValueById("model_tb_car_personal_id_card_no", "120311199606068814")
+           self.L.setValueById("model_tb_car_personal_nativeplace", "汉")
+           self.L.setValueById("model_tb_car_personal_current_address", "详细地址")
+           self.L.setValueById("model_tb_car_job_company_name", "单位全称")
+           self.L.setValueById("model_tb_car_job_company_address", "单位地址")
+           self.L.setValueById("model_tb_car_job_company_phone", "13214785214")
+           self.L.setValueById("model_tb_car_job_job_duty", "职位")
+           self.L.setValueById("model_tb_car_job_job_pay", "123654")
+           self.L.setValueById("model_tb_car_job_other_pay", "369852")
+           self.L.setValueById("multiple_customer_bank_info_bank_account", "62534442511225448")
+           self.L.setValueById("multiple_customer_bank_info_phone_number", "15876366685")
+           self.L.setValueById("multiple_customer_bank_info_bank_subname", "工商支行")
 
-            self.L.waitForSelectTextById('model_tb_car_personal_current_sheng', '广东省')
-            self.L.waitForSelectTextById('model_tb_car_personal_current_shi', '广州市')
-            self.L.waitForSelectTextById('model_tb_car_personal_current_xian', '东山区')
-            self.L.waitForSelectTextById('multiple_customer_bank_info_bank_name', '中国工商银行')
-            self.L.waitForSelectTextById('multiple_customer_bank_info_bank_provice', '广东省')
-            self.L.waitForSelectTextById('multiple_customer_bank_info_bank_city', '中山市')
-            self.L.waitForSelectTextById('multiple_customer_bank_info_output_type', '对私')
+           self.L.waitForSelectTextById('model_tb_car_personal_current_sheng', '广东省')
+           self.L.waitForSelectTextById('model_tb_car_personal_current_shi', '广州市')
+           self.L.waitForSelectTextById('model_tb_car_personal_current_xian', '东山区')
+           self.L.waitForSelectTextById('multiple_customer_bank_info_bank_name', '中国工商银行')
+           self.L.waitForSelectTextById('multiple_customer_bank_info_bank_provice', '广东省')
+           self.L.waitForSelectTextById('multiple_customer_bank_info_bank_city', '中山市')
+           self.L.waitForSelectTextById('multiple_customer_bank_info_output_type', '对私')
 
-            # 保存表单
-            self.driver.find_element_by_id('saveRow').click()
+        # 保存表单
+        self.driver.find_element_by_id('saveRow').click()
 
     # def tearDown(self):
         # self.driver.quit()

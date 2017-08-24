7c649bb88af263b250c4d4c5e4156ef68acc92ee
dragon8github <928532756@qq.com>
2017-08-24 16:01:49 +0800

    123

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
+# -*- coding:utf-8 -*-
 import pymysql
 
 #连接数据库
-conn = pymysql.connect(host='192.168.8.208', port=3306,user = 'root', passwd='tuandai_bm2015', db='tuandai_bm')
+conn = pymysql.connect(host='192.168.8.208', port=3306, user = 'root', passwd='tuandai_bm2015', db='tuandai_bm')
 
 #创建游标
 cur = conn.cursor()
 
+business_id = 'TDC1012017082402'
+
 #查询lcj表中存在的数据
-cur.execute("select * from tb_parameter where para_type='version' and para_name='H5'");
-
+cur.execute("select t2.`*`,t1.* from tb_process_approve as t1 left join tb_process_approve_user as t2 on t1.approve_id=t2.approve_id where business_id='"+ business_id +"';");
 #fetchall:获取lcj表中所有的数据
-ret1 = cur.fetchall()
+ret1 = cur.fetchone()
 
-print(ret1)
+print(ret1[1])
